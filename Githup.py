0db4901d6a04691ad9d8d55b535ea93fabd7b949
Uğur Ayebe <113903716+UgurAyebe0@users.noreply.github.com>
2022-09-25 14:27:11 +0300

    Add files via upload

---
+++ Githup.py
@@ -0,0 +1,27 @@
+İlk haftanın programlama dersi ödevi:
+    Github nedir ?
+        Github bir bütünün, çalışma ekibin internet tabanlı bir depolama servisini kullanarak
+        projelerini internet üstünden bir araya gelmeden beraber geliştirmelerini sağlamak üzere
+        aracı olan bir servistir. Ayrıca acık kaynak kodlu bir servis olması sebebi ile bircok yazilim,
+        ekibi tarafından kullanılmakta ve geliştirmeye acıktır. Githuba www.github.com adresinden ulaşabilirsiniz.
+        ayrıca gelişmelerden müjterilerde projemizi takipte kalabilir.
+    Ne amaçla kullanılır ?
+        Githup bireysel veya ekipce yapılan projelerde zamandan kazanılmak amaçı ile kullanılır. Ayrıca projeleri uzaktan kontrol 
+        edebilmek icinde kullanilir.
+    Yararları nedir ?
+        Pek çok kişiden oluşan bir ekipte (Fransa'dan Adıyaman'dan Danimarka'dan ayıca Tekirdağ'dan) oluşan bir ekipte
+        yüz yüze gelmeden ve yol ulaşım masraflarına girmeden uzaktan beraber proje yürütmelerinde olanak sağlayabilen
+        bir platform ve servistir. Bu sayede bir cok zaman kazanabilirler. 
+        Ayrıca müşterilerinde kullanabileceği bir alan sunabilir, mesela Ekibinizle hazırladığınız projeyi sunduktan sonra
+        müşteri proje üstünden bazı değişiklikler veya önermeler fikirler sunmak istiyor. Githup servisi üzerinden bu istekleri
+        düzenleyerek güzel bir şekilde müşteriye tekrar sunabilirsiniz ve bu sayede iletişimi daha hızlı sağlayarak vakitten kazanirsiniz.
+        Github'un gerçekten vakitten kat ve kat kazandırdığını sizlere söyleyebilirim.
+    Github ile ortak proje nasıl yazılır?
+        Öncelikle bir ekib olarak Github.com sitesinden bireysel hesap kuruyoruz.
+        Hesap kurduktan sonra proje yetkilisi githup ana menüsünden sağ yukarda + işeretine basıyoruz.
+        New Proje kısmına basıyoruz ve bir proje başlatıyoruz. Projeyi başlattıktan sonra,
+        Sol yukarıdan proje adını düzenleyebilirsiniz
+        Sağ yukarıda ise Anlayış, Proje detayla, Diğer seçenekler kısmında diğer seçekler kısmına giriyoruz.
+        ayarlar kısmına bastıktan sonra erişim yönetme sistemine giriyoruz, daha sonrasında ekip arkadaşlarımıza Rol verip projeye davet
+        ediyoruz.
+   
