54763929da56efa480b6dd40a1045596d605707b
Uğur Ayebe <113903716+UgurAyebe0@users.noreply.github.com>
2022-10-03 09:43:20 +0300

    Add files via upload

---
+++ 2lik 10luk.py
@@ -0,0 +1,38 @@
+   İkilik sayidan onluk sayi sistemine dönüştürmede 2 altarnatif var.
+        1. Altarnatif:
+          Sayi yazılır [(1100)2] bu sayının ikilik sayı sisteminde olduğunu ifade eder.
+          Mesela XCFGAS bunun gibi bir sayıyı şu şekilde onluk sisteme geçiririz..
+          s*iki üstü0+a*iki üstü bir+g* iki üstü iki+f*iki üstü üç+c*iki üstü dört+x* iki üstü 5
+          Bu şekilde gecirebilririz..
+          1100)2 =1*23+1*22+0*21+0*20
+          =8+4=(12)10 işlemin sonucudur.
+        2. Altarnatif:
+         1100 sayılarını sondan başlayarak 1 2 4 8  rakamlarının 1 olan yerdekiler toplanır yani
+         4+8= 12
+   Onluk sayidan ikilik sayi sistemine dönüştürmede 2 altarnatif vari.
+        1. Altarnatif:
+          Sayı her defasında 2'ye bölünmeye başlar ve bölüm sıfır olana kadar bu işlem 
+          devam eder ve sonucunda Kalan sayılar bir yere yazilir ve en altan en üste kalan sayılar
+          onluk sistemi ifade eder mesela
+          
+          İşlem   Bölüm  Kalan
+          155/2    77     1
+          77/2     38     1
+          38/2     19     0                 Mesela böyle bir işlemde onluk sisteme
+          19/2      9     1                 10011011 şeklinde yazilir...
+          9/2       4     1
+          4/2       2     0
+          2/2       1     0
+          1/2       0     1
+        2. Altarnatif:
+         1 2 4 8 16 32 64 128 256 512 1024 2048 4096 bu sayılar kutsaldır değişmez. şimdi farzi misal sayımız 582 yapılacak işlemleri inceleyin.
+            582-512 yapılır. ve sonuc 070 cıkar. 1 ekliyoruz.
+            Sayı 256 cıkarılamadiği için, 0 ekliyoruz.
+            Sayi 128 cıkarılamadiği için, 0 ekliyoruz.
+            70-64 yapılır ve sonuc 06 cıkar. 1 ekliyoruz.       Sayımızı en baştan en sona doğru yaziyoruz.
+            Sayi 32 cıkarılamadiği için, 0 ekliyoruz.           1001000110 olacak şekilde dönüştürdük.
+            Sayi 16 cıkarılamadiği için, 0 ekliyoruz.           
+            Sayi 8 cıkarılamadiği için, 0 ekliyoruz.
+            6-4 yapılır. ve sonuc 2 cıkar. 1 ekliyoruz.
+            2-2 yapılır. ve sonuc 0 cıkar. 1 ekliyoruz
+            Sayi 1 cıkarılamadiği için, 0 ekliyoruz.
