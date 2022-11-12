6ab9be0fc336491f26ba2c28348a6cbd253b3e2a
Uğur Ayebe <uurayeem@gmail.com>
2022-11-12 14:15:26 +0300

    Sayı Değeri
    Çift veya tek sayıları hedefleyen bir program

---
+++ Ciftmi Tek mi/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b = 2,mod;
+            Console.WriteLine("Bir sayı giriniz");
+            a = Convert.ToInt32(Console.ReadLine());
+            mod = ( a % b);
+            if (mod == 0)
+                Console.WriteLine("Girilen sayi çift sayıdır.");
+            else 
+                Console.WriteLine("Girilen sayi tek sayidir.");
+            Console.ReadKey();
+        }
+    }
+}
