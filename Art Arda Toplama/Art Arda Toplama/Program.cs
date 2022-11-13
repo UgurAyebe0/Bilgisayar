b0e132a35322df97ad11d1c845b7144bcb55e3de
Uğur Ayebe <uurayeem@gmail.com>
2022-11-13 21:49:39 +0300

    Art Arda Toplama
    sayi 83 olana kadar döngü sürecektir.

---
+++ Art Arda Toplama/Art Arda Toplama/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Art_Arda_Toplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int sayi1, sayi2;
+                Console.WriteLine("Ana Sayiyi Giriniz:");
+                sayi1 = Convert.ToInt32(Console.ReadLine());
+            Tekrar:
+                Console.WriteLine("Toplanacak sayıyı girin:");
+                sayi2 = Convert.ToInt32(Console.ReadLine());
+                sayi1 = sayi1 + sayi2;
+                Console.WriteLine("Sayımız" + sayi1);
+                if (sayi1 == 83)
+                    Console.WriteLine("Döngü Bitti.");
+                else
+                    goto Tekrar;
+            }
+            Console.ReadKey();
+        }
+    }
+}
