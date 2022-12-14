ad5cba376ede6c8161fe7ccde407853baaeb8f50
Uğur Ayebe <uurayeem@gmail.com>
2022-12-14 12:52:44 +0300

    Ödevim
    ÖDEV: Öyle bir program yazın ki, önce kullanıcıya kaç adet sayı girmek istediğini sorsun. Daha sonra kullanıcıdan sayı
    girmesini istesin. Kullanıcı fark etse de etmese de baştan girdiği adet kadar sayı tamamlandığında bu sayıların toplamını
    ve ortalamasını hesaplayarak ekrana yazsın.

---
+++ Girilen Sayı Kadar Sayı Toplama/Girilen Sayı Kadar Sayı Toplama/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace or091
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, c, d = 0, f, e;
+            Console.WriteLine("Kaç Adet Sayi Girmek İstediğinizi Yazin");
+            a = Convert.ToInt32(Console.ReadLine());
+            f = a;
+            do
+            {
+                Console.WriteLine("Bir Sayi giriniz" + "(" + a + ")");
+                c = Convert.ToInt32(Console.ReadLine());
+                a--;
+                d += c;
+            } while (a > 0);
+            e = d / f;
+            Console.WriteLine("Girdiğiniz Sayıların Toplamı: " + d);
+            Console.WriteLine("Girdiğiniz Sayıların Ortalaması: " + e);
+        }
+    }
+}
---
+++ Girilen Sayı Kadar Sayı Toplama/Girilen Sayı Kadar Sayı Toplama/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Girilen Sayı Kadar Sayı Toplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Girilen Sayı Kadar Sayı Toplama")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("07b56133-3466-427c-a3c3-234f6d7ebd72")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
