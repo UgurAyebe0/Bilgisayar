dd7aa660e70e51bc1e6a1accd7584c190aafbbbe
Uğur Ayebe <uurayeem@gmail.com>
2022-12-14 13:15:23 +0300

    Kodu Anlama
    Sayfa 79da kodda ufak değişiklik yapmamız istenmişti. Yaptik

---
+++ Kod Okumak Sayfa 79/Kod Okumak Sayfa 79/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace or094
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a,b;
+            Console.Write("Bir Sayı Giriniz");
+            b = Convert.ToInt32(Console.ReadLine());
+            String s = "b Sayısı Asal Sayı";
+            if (b == 1)
+            {
+                Console.WriteLine("ASAL DEĞİL!");
+                goto bitir; }
+            for (a = 2; a <= b / 2; a++)
+            {
+                if (b % a == 0)
+                {
+                    s = "b Sayısı Asal Sayı Değil!";
+                    break;
+                }
+            }
+            Console.WriteLine(s);
+        bitir: Console.Write("DEVAM ETMEK İÇİN BİR TUŞA BASINIZ. . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Kod Okumak Sayfa 79/Kod Okumak Sayfa 79/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Kod Okumak Sayfa 79")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Kod Okumak Sayfa 79")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a7e37b7c-dbb2-4cc1-9630-f52d410e9c0e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
