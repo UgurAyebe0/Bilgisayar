add00dd26c7c8ab03bdc22f5a65ce37fc67339c1
Uğur Ayebe <uurayeem@gmail.com>
2023-01-05 11:21:07 +0300

    Güncelleme
    Güncelleme

---
+++ Sınav Sorusu B/Sınav Sorusu B/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] b = { 0, 0, 0 };
+            int a;
+            string s = " ";
+            string[] Birler = { "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz" };
+            string[] Onlar = { "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan" };
+            string[] Yüzlük = { "Yüz", "İki Yüz", "Üç Yüz", "Dört Yüz", "Beş Yüz", "Alti Yüz ", "Yedi Yüz", "Sekiz Yüz", "Dokuz Yüz" };
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Bir Sayı Giriniz:");
+                b[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (b[i] / 100 >= 1)
+                {
+                    Console.Write(Yüzlük[((b[i] / 100) - 1)]);
+                    b[i] = b[i] % 100;
+                }
+                if (b[i] / 10 >= 1)
+                {
+                    Console.Write(Onlar[((b[i] / 10) - 1)]);
+                    b[i] = b[i] % 10;
+
+                }
+                if (b[i] % 10 != 0)
+                {
+                    Console.Write(Birler[(b[i] - 1)]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
