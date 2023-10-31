86ffee0dcf3b9ccf8dd5b6d897599411c214bade
Uğur Ayebe <113903716+UgurAyebe0@users.noreply.github.com>
2023-10-31 10:23:14 +0300

    Update Program.cs
    s

--- Vize/Sınav Sorusu/Program.cs
+++ Vize/Sınav Sorusu/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApp4
             int hesaplanan_saat;
             int ödeme = 0;
             Console.WriteLine("Kaç saat girdiğinizi yazın");
-            girilen_saat = Convert.ToInt16(Console.ReadLine());
+            girilen_saat = Convert.ToInt32(Console.ReadLine());
 
             hesaplanan_gün = girilen_saat / 24;
             hesaplanan_saat = girilen_saat % 24;
@@ -27,7 +27,7 @@ namespace ConsoleApp4
             {
                 ödeme = (hesaplanan_gün * 25) + 6;
             }
-            if (hesaplanan_saat >=2 && hesaplanan_saat <= 8)
+            if (hesaplanan_saat >= 2 && hesaplanan_saat <= 8)
             {
                 hesaplanan_saat = hesaplanan_saat - 2;
                 ödeme = (hesaplanan_gün * 25) + 6 + (hesaplanan_saat * 2);
@@ -36,7 +36,7 @@ namespace ConsoleApp4
             {
                 ödeme = (hesaplanan_gün + 1) * 25;
             }
-            Console.WriteLine("{0} Plakalı aracınız için yapacağınız ödeme {1} TL", ödeme);
+            Console.WriteLine("Yapacağınız ödeme {0} TL", ödeme);
         }
     }
 }
